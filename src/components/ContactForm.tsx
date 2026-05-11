d473d122bb92fe3ce2b305144d80eb2631c2a64f
Brullie88 <martijnbruls@gmail.com>
2026-05-11 21:14:03 +0200

    Update ContactForm.tsx

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -18,7 +18,7 @@ export default function ContactForm() {
                 Liever bellen?
               </h3>
               <p className="text-levanto-gray mb-6 font-medium">
-                Snel antwoord op uw vragen:
+                Snel antwoord/reactie op uw vragen:
               </p>
               <a href="tel:+31045121212" className="text-3xl lg:text-4xl font-display font-bold text-levanto-magenta hover:text-levanto-gold transition-colors">
                 045 - 121212
